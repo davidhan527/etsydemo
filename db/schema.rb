78f1a2fa8566b25c2a0f7363203946c73011fece
davidhan527 <davidhan527@gmail.com>
2014-07-12 13:35:35 -0400

    >_: rails generate scaffold Order address:string city:string state:string >_: rails generate migration AddListingIdToOrders listing_id:integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705231121) do
+ActiveRecord::Schema.define(version: 20140712173417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20140705231121) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "listing_id"
   end
 
   create_table "users", force: true do |t|
