dc1b31ca0ba4ad913f09616a3836607d7398389f
davidhan527 <davidhan527@gmail.com>
2014-07-12 15:08:46 -0400

    adding foreign key fields for user order relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712173417) do
+ActiveRecord::Schema.define(version: 20140712190758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20140712173417) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "listing_id"
+    t.integer  "buyer_id"
+    t.integer  "seller_id"
   end
 
   create_table "users", force: true do |t|
